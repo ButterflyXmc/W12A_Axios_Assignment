aeef5b2a11e519e3271a58eb5e68560a79d4b9ad
mahic <mahic@domain.com>
2022-11-23 17:24:49 -0700

    finished js

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const quote_button = document.getElementById(`quote_button`);
+const result = document.getElementById(`result`);
+
+function quoteSuccess(response){
+    let data = response.data
+    result.innerHTML = `<h2>${data.quote}</h2>`
+}
+
+function quoteFailure(error) {
+    result.innerHTML = `<h2>An error has occured</h2?`
+}
+
+function getRandomQuote(){
+    axios.request({
+    url : "https://api.kanye.rest"
+}).then(quoteSuccess).catch(quoteFailure);
+}
+
+///quote changes everytime 
+quote_button.addEventListener(`click`, getRandomQuote )
+
+
+
+
